3a533d0b5ea2a924827b874701c447195d51be52
Maximiliano Millan <maxi_millan23@hotmail.com>
2022-10-19 10:32:25 -0300

    Agregando ;

--- js/Display.js
+++ js/Display.js
@@ -12,19 +12,19 @@ class Display{
             multiplicar: 'x',
             restar: '-',
         }
-    }
+    };
 
     borrar(){
         this.valorActual = this.valorActual.toString().slice(0,-1);
         this.imprimirValores();
-    }
+    };
 
     borrarTodo(){
         this.valorActual = '';
         this.valorAnterior = '';
         this.tipoOperacion = undefined;
         this.imprimirValores();
-    }
+    };
 
     computar(tipo){
         this.tipoOperacion !== 'igual' && this.calcular();
@@ -32,18 +32,18 @@ class Display{
         this.valorAnterior = this.valorActual || this.valorAnterior;
         this.valorActual = '';
         this.imprimirValores();
-    }
+    };
 
     agregarNumero(numero){
         if(numero === '.' && this.valorActual.includes('.'))return
         this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
-    }
+    };
 
     imprimirValores(){
         this.displayValorActual.textContent = this.valorActual;
         this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;/*Si se preciona el igual que muestre un string vacio*/
-    }
+    };
 
     calcular(){
         const valorAnterior = parseFloat(this.valorAnterior);
@@ -52,5 +52,5 @@ class Display{
         if(isNaN (valorActual) || isNaN(valorAnterior)) return
         this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
 
-    }
-}
+    };
+};
